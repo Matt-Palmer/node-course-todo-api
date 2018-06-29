993da76fac89321e431d4c0e9e04b892c7b73142
Matt Palmer <mpalmer919@Matts-MacBook-Pro.local>
2018-06-29 12:26:04 +0100

    Test post todos route

--- server/server.js
+++ server/server.js
@@ -9,22 +9,9 @@ let app = express();
 
 app.use(bodyParser.json());
 
-// app.post('/todos', (req, res) => {
-//     let todo = new Todo({
-//         text: req.body.text
-//     });
-//
-//     todo.save().then((doc) => {
-//         res.send(doc);
-//     }, (err) => {
-//         res.status(400).send(err);
-//     });
-// })
-
 app.post('/todos', (req, res) => {
     let todo = new Todo({
-        text: req.body.text,
-        completed: req.body.completed
+        text: req.body.text
     });
 
     todo.save().then((doc) => {
@@ -37,3 +24,8 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
     console.log('Started on port 3000');
 })
+
+
+module.exports = {
+    app
+};
