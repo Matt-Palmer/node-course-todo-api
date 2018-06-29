7424fcdc9544317c9ea72ad2973a94f53a22efee
Matt Palmer <mpalmer919@Matts-MacBook-Pro.local>
2018-06-29 21:57:45 +0100

    Added remove

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,25 @@
+const {mongoose} = require('./../server/db/mongoose.js');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+const {ObjectId} = require('mongodb');
+
+// With .remove({}), an empty object needs to be passed in to have any effect.
+// Just calling .remove() with no arguments will do nothing.
+//
+// Todo.remove({}).then((result) => {
+//     console.log(result);
+// });
+
+
+// findOneAndRemove() and findByIdAndRemove() both work in similar ways, the only difference being
+// how much detail is entered into the argument, with findOneAndRemove() we need to pass in an object
+// with a key value pair for the key we wish to find. With findByIdAndRemove() we only need to
+// enter a string matching an items id.
+//
+// Todo.findOneAndRemove({_id: "5b369740ee7f39ce6a70f331"}).then((result) => {
+//     console.log(result);
+// });
+//
+// Todo.findByIdAndRemove('5b369740ee7f39ce6a70f331').then((result) => {
+//     console.log(result);
+// });
